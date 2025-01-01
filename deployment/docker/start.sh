a472d11fed0a6b2b5452ec55c73017c8cf98e70a
zach-planet-nine <zach@planetnine.app>
2025-01-01 14:51:39 -0800

    new services

--- deployment/docker/start.sh
+++ deployment/docker/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-nohup nodemon /usr/src/app/julia/src/server/node/server.js &
-nohup nodemon /usr/src/app/continuebee/src/server/node/server.js &
-nohup nodemon /usr/src/app/joan/src/server/node/server.js &
-nohup nodemon /usr/src/app/pref/src/server/node/server.js &
-nohup nodemon /usr/src/app/bdo/src/server/node/server.js &
-nohup nodemon /usr/src/app/fount/src/server/node/server.js &
-nohup nodemon /usr/src/app/addie/src/server/node/server.js &
+pm2 start /usr/src/app/julia/src/server/node/server.js &
+pm2 start /usr/src/app/continuebee/src/server/node/server.js &
+pm2 start /usr/src/app/joan/src/server/node/server.js &
+pm2 start /usr/src/app/pref/src/server/node/server.js &
+pm2 start /usr/src/app/bdo/src/server/node/server.js &
+pm2 start /usr/src/app/fount/src/server/node/server.js &
+pm2 start /usr/src/app/addie/src/server/node/server.js &
+pm2 start /usr/src/app/aretha/src/server/node/server.js &
+pm2 start /usr/src/app/sanora/src/server/node/server.js &
 
 # Keep the script running
 tail -f /dev/null
