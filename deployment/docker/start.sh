3719defcabb00cebdfec2726c86f7e57f64e56a7
zach-planet-nine <zach@planetnine.app>
2025-01-01 16:03:06 -0800

    localhost

--- deployment/docker/start.sh
+++ deployment/docker/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-pm2 start /usr/src/app/julia/src/server/node/server.js &
-pm2 start /usr/src/app/continuebee/src/server/node/server.js &
-pm2 start /usr/src/app/joan/src/server/node/server.js &
-pm2 start /usr/src/app/pref/src/server/node/server.js &
-pm2 start /usr/src/app/bdo/src/server/node/server.js &
-pm2 start /usr/src/app/fount/src/server/node/server.js &
-pm2 start /usr/src/app/addie/src/server/node/server.js &
-pm2 start /usr/src/app/aretha/src/server/node/server.js &
-pm2 start /usr/src/app/sanora/src/server/node/server.js &
+LOCALHOST=true pm2 start /usr/src/app/julia/src/server/node/server.js &
+LOCALHOST=true pm2 start /usr/src/app/continuebee/src/server/node/server.js &
+LOCALHOST=true pm2 start /usr/src/app/joan/src/server/node/server.js &
+LOCALHOST=true pm2 start /usr/src/app/pref/src/server/node/server.js &
+LOCALHOST=true pm2 start /usr/src/app/bdo/src/server/node/server.js &
+LOCALHOST=true pm2 start /usr/src/app/fount/src/server/node/server.js &
+LOCALHOST=true pm2 start /usr/src/app/addie/src/server/node/server.js &
+LOCALHOST=true pm2 start /usr/src/app/aretha/src/server/node/server.js &
+LOCALHOST=true pm2 start /usr/src/app/sanora/src/server/node/server.js &
 
 # Keep the script running
 tail -f /dev/null
