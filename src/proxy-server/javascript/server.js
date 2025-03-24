ea03912b8febbacd88157bcca3468ce569fae3ff
zach-planet-nine <zach@planetnine.app>
2025-03-24 16:59:09 -0700

    proxy server for demo

--- src/proxy-server/javascript/server.js
+++ src/proxy-server/javascript/server.js
@@ -4,6 +4,9 @@ import cors from 'cors';
 import fount from 'fount-js';
 import bdo from 'bdo-js';
 
+fount.baseURL = 'http://localhost:3006/';
+bdo.baseURL = 'http://localhost:3003/';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,12 +16,12 @@ const hash = 'tiddly-proxy';
 let keys;
 let allyabaseUser;
 
-const stateJSON = fs.readFileSync('./store', {encoding: 'utf-8'});
+/*const stateJSON = fs.readFileSync('./store', {encoding: 'utf-8'});
 if(stateJSON) {
   const state = JSON.parse(stateJSON);
   keys = state.keys;
   allyabaseUser = state.allyabaseUser;
-}
+}*/
 
 // Here we proxy some stuff.
 
@@ -35,14 +38,37 @@ console.log('req received');
 
     allyabaseUser.bdoUUID = bdoUUID;
     allyabaseUser.fountUUID = fountUser.uuid;
+    allyabaseUser.fountUser = fountUser;
 
     const state = {keys, allyabaseUser};
-    fs.writeFileSync('./store', JSON.stringify(state));
+//    fs.writeFileSync('./store', JSON.stringify(state));
+  } else {
+    allyabaseUser.fountUser = await fount.getUserByUUID(allyabaseUser.fountUser.uuid);
+    allyabaseUser.fountUser.nineum = await fount.getNineum(allyabaseUser.fountUser.uuid);
+console.log('allyabaseUser.fountUser with nineum', allyabaseUser.fountUser);
   }
 console.log('sending back', allyabaseUser);
 
   res.send(allyabaseUser);
 }); 
+
+app.get('/register-for-class', async (req, res) => {
+console.log('registerin');
+  if(!allyabaseUser) {
+    res.status(404);
+    return res.send({error: 'no allyabase user'});
+  }
+
+  const success = fetch(`http://localhost:4444/plugin/ftt/abelard?uuid=${allyabaseUser.fountUUID}&host=${encodeURIComponent('http://localhost:8081/create-allyabase-user')}`);
+  if(!success.success) {
+console.log('success!');
+    return res.send(success);
+  }
+
+console.log('no dice');
+  res.status(404);
+  return res.send({error: 'not found'});
+});
 
 app.get('/grab-fed-wiki-user', async (req, res) => {
   
